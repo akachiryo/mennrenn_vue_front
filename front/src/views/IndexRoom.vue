a40b60f0cf12b297f3419f3cc2cd885680f37c53
akachiryo <rirugameri@icloud.com>
2022-01-28 21:20:56 +0900

    タグ検索　完了

--- front/src/views/IndexRoom.vue
+++ front/src/views/IndexRoom.vue
@@ -1,5 +1,33 @@
 <template>
   <v-container>
+    <template>
+      <v-card class="mb-3">
+        <v-card-text>
+          <header>絞り込み条件</header>
+          <v-row dense justify="start">
+            <template>
+              <v-checkbox v-for="tag in tags"
+                          :key="tag.id"
+                          v-model="query.selectedTags"
+                          :label="tag.name"
+                          :value="tag.id"
+                          class="mr-3"
+                          @click="fetchRooms"
+                          hide-details="auto"
+              >
+              </v-checkbox>
+            </template>
+          </v-row>
+          <v-row>
+            <template>
+              <v-col cols="12">
+                <v-text-field label="RoomTitle" v-model="query.roomTitle" @input="fetchRooms"></v-text-field>
+              </v-col>
+            </template>
+          </v-row>
+        </v-card-text>
+      </v-card>
+    </template>
     <v-row
      justify="center"
      align-content="center">
@@ -21,6 +49,7 @@
 
 <script>
 import axios from 'axios';
+import qs from 'qs';
 import RoomItem from '../components/RoomItem.vue';
 
 export default {
@@ -28,6 +57,11 @@ export default {
     return {
       rooms: [],
       room: {},
+      tags: [],
+      query: {
+          selectedTags: [],
+          roomTitle: ''
+      },
       pagingMeta: null,
       currentPage: 1
     };
@@ -36,32 +70,42 @@ export default {
     RoomItem
   },
   created() {
-    },
-  mounted() {
     this.fetchRooms()
-    this.setRooms();
-  },
+    this.fetchTags()
+    },
   computed: {
     isExistRooms() {
       return this.rooms.length > 0;
     }
   },
   methods: {
-    setRooms() {
-      this.$emit('loadingTrue');
-      axios
-        .get('http://localhost:3000/api/rooms')
-        .then((response) => {
-          this.rooms = response.data;
-          this.$emit('loadingFalse');
-        })
-        .catch((error) => console.log(error));
-    },
-    async fetchRooms() {
-        const res = await axios.get(`http://localhost:3000/api/rooms`, { params: { page: this.currentPage } })
-        this.rooms = res.data.rooms
-        this.pagingMeta = res.data.meta
-    },
+     async fetchRooms() {
+      //  this.$emit('loadingTrue');
+              const searchParams = {
+                  q: {
+                      title: this.query.roomTitle,
+                      tag_ids: this.query.selectedTags
+                  }
+              }
+              const pagingParams = { page: this.currentPage }
+              const params = { ...searchParams, ...pagingParams }
+              const paramsSerializer = (params) => qs.stringify(params, { arrayFormat: 'brackets' });
+              const res = await axios.get(`http://localhost:3000/api/rooms`, { params, paramsSerializer })
+              this.rooms = res.data.rooms
+              this.pagingMeta = res.data.meta
+              // this.$emit('loadingFalse');
+          },
+    // async fetchRooms() {
+    //     this.$emit('loadingTrue');
+    //     const res = await axios.get(`http://localhost:3000/api/rooms`, { params: { page: this.currentPage } })
+    //     this.rooms = res.data.rooms
+    //     this.pagingMeta = res.data.meta
+    //     this.$emit('loadingFalse');
+    // },
+     async fetchTags() {
+              const res = await axios.get(`http://localhost:3000/api/tags`)
+              this.tags = res.data
+          },
     paging(page) {
       this.currentPage = page
       this.fetchRooms()
