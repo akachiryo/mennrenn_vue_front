e5022e9625d1175b2101f053a53474fe88f1a63e
akachiryo <rirugameri@icloud.com>
2022-04-06 02:00:32 +0900

    質問リストに追加可能

---
+++ front/src/components/QuestionLists.vue
@@ -0,0 +1,101 @@
+<template>
+  <div>
+    <p>{{ selected }}</p>
+    <v-list>
+      <v-list-item>
+        <v-list-item-title>質問リスト</v-list-item-title>
+        <v-list-item-action>
+          <v-list-item-action-text>
+            <v-btn
+              class="ma-2"
+              tile
+              outlined
+              color="success"
+              @click="$refs.dialog.open()"
+            >
+              <v-icon left>mdi-pencil</v-icon>質問作成
+            </v-btn>
+          </v-list-item-action-text>
+        </v-list-item-action>
+      </v-list-item>
+      <v-list-group
+        v-for="question in questions"
+        :key="question.id"
+      >
+        <template v-slot:activator >
+          <v-list-item-title v-text="question.content"></v-list-item-title>
+          <v-list-item-action>
+            <v-btn
+              x-small
+              fab
+              dark
+              color="error lighten-1"
+              @click="deleteRoom(question.id)"
+              v-if="isAdmin"
+            >
+            <v-icon dark>mdi-delete</v-icon>
+            </v-btn>
+          </v-list-item-action>
+        </template>
+        
+        <v-list-item>
+          <v-list-content>
+            <v-list-subtitle>
+              <v-checkbox
+              v-for="child in question.questions"
+              :key="child.id"
+              v-model="selected"
+              :label="child.content"
+              :value="child.content"
+              >
+              </v-checkbox>
+            </v-list-subtitle>
+          </v-list-content>
+        </v-list-item>
+      </v-list-group>
+    </v-list>
+    <create-question-modal @fetchQuestions="fetchQuestions" ref="dialog"></create-question-modal>
+  </div>
+
+</template>
+
+<script>
+import axios from 'axios'
+import CreateQuestionModal from './CreateQuestionModal.vue'
+
+export default {
+  data() {
+    return {
+      questions: null,
+      selected: []
+    }
+  },
+  components: {
+    CreateQuestionModal
+  },
+  created() {
+    this.fetchQuestions()
+  },
+  computed: {
+    isAdmin() {
+      return this.$store.getters['auth/currentUser'].admin;
+    },
+  },
+  methods: {
+    async fetchQuestions() {
+      axios.defaults.baseURL = process.env.VUE_APP_API_ENDPOINT;
+      axios.get(`/api/v1/question_types`).then((response) => {
+        this.questions = response.data;
+      });
+    },
+     async deleteRoom(questionId) {
+      axios.defaults.baseURL = process.env.VUE_APP_API_ENDPOINT;
+      if (confirm('削除しますか？')) {
+        await axios.delete(`/api/v1/question_types/${questionId}`);
+        this.$router.push('/admin/questions');
+      }
+      this.fetchQuestions()
+    },
+  }
+}
+</script>
